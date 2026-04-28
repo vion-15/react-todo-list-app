257f7547077c71c1d9e189ea29c14a5d1ad558f4
vion-15 <ahmadfaidh2003@gmail.com>
2026-04-28 11:57:00 +0700

    Create modal form

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,45 @@
+import { useState } from "react"
 import "./style/Header.css"
 
 export default function Header() {
+
+    const [showModal, setShowModal] = useState(false);
+
+    function handleModal() {
+        setShowModal(true);
+    };
+
+
     return (
-        <div className="header">
-            <div className="title">
-                <button className="material-symbols-outlined bgrMenu">
-                    menu
+        <>
+            <div className="header">
+                <div className="title">
+                    <span> | </span>
+                    <span>Dashboard</span>
+                </div>
+                <button className="material-symbols-outlined btnAdd" onClick={handleModal}>
+                    add
                 </button>
-                <span> | </span>
-                <span>Dashboard</span>
             </div>
-            <button className="material-symbols-outlined btnAdd">
-                add
-            </button>
-        </div>
+
+            <section>
+                {showModal && (
+                    <div className="modal">
+                        <form action="">
+                            <input type="text" placeholder="Title..." />
+                            <div>
+                                <input type="text" placeholder="Task..." />
+                                <button>Add</button>
+                            </div>
+                        </form>
+                        <div className="allTask">
+                            <h4>All Task :</h4>
+                        </div>
+                        <button>Save</button>
+                    </div>
+                )}
+            </section>
+
+        </>
     )
 }
