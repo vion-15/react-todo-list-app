ecdcf587306cced41368890cca896a70027cd074
vion-15 <ahmadfaidh2003@gmail.com>
2026-04-29 09:54:04 +0700

    add task logic

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,10 +4,27 @@ import "./style/Header.css"
 export default function Header() {
 
     const [showModal, setShowModal] = useState(false);
+    const [task, setTask] = useState("");
+    const [tasks, setTasks] = useState([]);
+
+    const handleChange = (e) => {
+        setTask(e.target.value);
+    }
+
+    const addTask = (e) => {
+        e.preventDefault();
+        setTasks([...tasks, task]);
+        setTask("");
+    }
 
     function handleModal() {
         setShowModal(true);
     };
+
+    const deleteTask = (i) => {
+        const newTasks = tasks.filter((_, index) => index !== i)
+        setTasks(newTasks);
+    }
 
 
     return (
@@ -28,12 +45,22 @@ export default function Header() {
                         <form action="">
                             <input type="text" placeholder="Title..." />
                             <div>
-                                <input type="text" placeholder="Task..." />
-                                <button>Add</button>
+                                <input type="text" placeholder="Task..." value={task} onChange={handleChange} />
+                                <button onClick={addTask}>Add</button>
                             </div>
                         </form>
                         <div className="allTask">
                             <h4>All Task :</h4>
+                            <ol>
+                                {tasks.map((t, i) => (
+                                    <li key={i}>
+                                        <div className="item">
+                                            {t}
+                                            <button onClick={() => deleteTask(i)}>X</button>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ol>
                         </div>
                         <button>Save</button>
                     </div>
